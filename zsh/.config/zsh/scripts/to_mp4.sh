0d564985c6e3efa8dc5d7bdb969bb34dc17d7f6d
Luis Lacruz <luixlacrux@users.noreply.github.com>
2025-04-13 16:31:12 -0500

    feat: Remove to_mp4 alias and delete associated script

--- zsh/.config/zsh/scripts/to_mp4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-FILE=$1
-SPEED=$2
-FILENAME=$(basename "$FILE")
-DIRNAME=$(dirname "$FILE")
-FNAME="${FILENAME%.*}"
-ffmpeg -i "$FILE" -an -filter:v "setpts=PTS/${SPEED:-1}" "$DIRNAME/$FNAME-1.mp4"
